9f1e9d1b25d60798f3b8dfc352bd2128f8e04350
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-01-28 16:47:17 -0700

    Increase coverage

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,12 +1,15 @@
 import os
 import subprocess
 
+import pytest
+
 here = os.path.abspath(os.path.dirname(__file__))
 
 
-def test_cli_runner():
+@pytest.mark.parametrize('v', ['--verbose', '--no-verbose'])
+def test_cli_runner(v):
     esmcol_file = os.path.join(here, 'test_data/good_collection.json')
-    cmd = ['esmcol-validator', esmcol_file, '--timer', '--verbose', '--log-level', 'DEBUG']
+    cmd = ['esmcol-validator', esmcol_file, '--timer', v, '--log-level', 'DEBUG']
     p = subprocess.Popen(cmd, stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL,)
 
     p.communicate()
